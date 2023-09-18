02fdec69150e3a6ac6712b3a1e31273e4fb31494
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-09-18 10:56:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,2 @@
 //your JS code here. If required.
-function secondHighest(arr) {
-    if (arr.length < 2) {
-        return -Infinity;
-    }
-    
-    let firstMax = -Infinity;
-    let secondMax = -Infinity;
-    
-    for (let num of arr) {
-        if (num > firstMax) {
-            secondMax = firstMax;
-            firstMax = num;
-        } else if (num < firstMax && num > secondMax) {
-            secondMax = num;
-        }
-    }
-    
-    if (secondMax === -Infinity) {
-        return -Infinity;
-    }
-    
-    return secondMax;
-}
+
